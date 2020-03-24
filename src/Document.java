67b863f6027b6d7b123f2fb7878b1ff4d0c3c137
Gohansupersaiyan12 <habershamfrank@yahoo.com>
2020-03-24 00:34:38 -0400

    completed code for Document class

--- src/Document.java
+++ src/Document.java
@@ -1,4 +1,24 @@
 
 public class Document {
-
+	private String name;
+	public Document(String name) {
+		this.name = name;
+		System.out.println("Documented named "+this.name+" is created");
+	}
+	
+	public void load() {
+		System.out.println("document "+this.name+" is loaded into view");
+	}
+	
+	public void save() {
+		System.out.println("document "+this.name+" is being saved...");
+	}
+	
+	public void spellCeck() {
+		System.out.println("documen "+this.name+" is being checked for spelling errors");
+	}
+	
+	public void print() {
+		System.out.println("document "+this.name+" is printing...");
+	}
 }
